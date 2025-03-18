965872d896d3e2425a6e0f748f04d5781510e025
FrimcX <181182782+FrimcX@users.noreply.github.com>
2025-03-18 17:51:51 -0300

    Create getComputerChoice() function to get a random string value
    This value will be the choice of the computer (our opponent)

--- js.js
+++ js.js
@@ -1,1 +1,16 @@
 console.log("Hello World!");
+
+/*getComputerChoice() function will randomly return one of the following strings values: "rock", "paper" or "scissors"*/
+
+function getComputerChoice() {
+    let choiceNum = Math.floor(Math.random()*100);
+    if (choiceNum <= 32) {
+        return "rock"  + " - " + choiceNum;
+    } else if (choiceNum <= 66) {
+        return "paper"  + " - " + choiceNum;
+    } else if (choiceNum <= 99) {
+        return "scissors" + " - " + choiceNum;
+    }
+}
+
+console.log(getComputerChoice());
