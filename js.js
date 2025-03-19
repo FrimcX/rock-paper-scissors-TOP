6e48f05fe74dd7427956cddb82c4297b10774a28
FrimcX <181182782+FrimcX@users.noreply.github.com>
2025-03-18 23:13:09 -0300

    Create playGame() function to connect the dots and get the final result
    The function playGame() calls the playRound() function 5 times,
    it also keeps track of the score, and prints the results in a
    structured manner

--- js.js
+++ js.js
@@ -13,49 +13,83 @@ function getComputerChoice() {
 
 /*getHumanChoice() will be the function that takes the user choice and returns it */
 function getHumanChoice(string) {
-    let humanChoice = string; 
+    let humanChoice = string.toLowerCase(); //humanChoice but case insensitive
     return humanChoice;
 }
 
-function playRound(humanChoice, computerChoice) {
-    let humanChoiceData = humanChoice.toLowerCase(); //humanChoice but case insensitive
-    console.log("Human: " + humanChoiceData); //Checking results
-    console.log("Computer: " + computerChoice); //Checking results
 
-    if (humanChoiceData == "rock") {
-        if (computerChoice == "rock") {
-            console.log("It' a tie!");
-        } else if (computerChoice == "paper") {
-            console.log("You lose! Paper beats Rock...");
-        } else if (computerChoice == "scissors") {
-            console.log("You win! Rock beats Scissors...");
-        }
-    } else if (humanChoiceData == "paper") {
-        if (computerChoice == "rock") {
-            console.log("You win! Paper beats Rock...");
-        } else if (computerChoice == "paper") {
-            console.log("It' a tie!");
-        } else if (computerChoice == "scissors") {
-            console.log("You lose! Scissors beats Paper...");
-        }
-    } else if (humanChoiceData == "scissors") {
-        if (computerChoice == "rock") {
-            console.log("You lose! Rock beats Scissors...");
-        } else if (computerChoice == "paper") {
-            console.log("You win! Scissors beats Paper...");
-        } else if (computerChoice == "scissors") {
-            console.log("It' a tie!");
+
+function playGame() {
+    //variables to save the score 
+    let humanScore = 0; 
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        console.log("Human: " + humanChoice + " | Computer: " + computerChoice); //checking choices
+
+        if (humanChoice == "rock") {
+            if (computerChoice == "rock") {
+                console.log("It' a tie!");
+            } else if (computerChoice == "paper") {
+                console.log("You lose! Paper beats Rock...");
+                computerScore += 1;
+            } else if (computerChoice == "scissors") {
+                console.log("You win! Rock beats Scissors...");
+                humanScore += 1;
+            }
+        } else if (humanChoice == "paper") {
+            if (computerChoice == "rock") {
+                console.log("You win! Paper beats Rock...");
+                humanScore += 1;
+            } else if (computerChoice == "paper") {
+                console.log("It' a tie!");
+            } else if (computerChoice == "scissors") {
+                console.log("You lose! Scissors beats Paper...");
+                computerScore += 1;
+            }
+        } else if (humanChoice == "scissors") {
+            if (computerChoice == "rock") {
+                console.log("You lose! Rock beats Scissors...");
+                computerScore += 1;
+            } else if (computerChoice == "paper") {
+                console.log("You win! Scissors beats Paper...");
+                humanScore += 1;
+            } else if (computerChoice == "scissors") {
+                console.log("It' a tie!");
+            }
         }
     }
+
+    console.group("<-- Game 1 -->");
+    playRound(getHumanChoice(prompt("rock, paper or scissors?")), getComputerChoice());
+    console.log("Human: " + humanScore + " | Computer: " + computerScore);
+    console.groupEnd();
+    console.log("\n");
+
+
+    console.group("<-- Game 2 -->");
+    playRound(getHumanChoice(prompt("rock, paper or scissors?")), getComputerChoice());
+    console.log("Human: " + humanScore + " | Computer: " + computerScore);
+    console.groupEnd();
+    console.log("\n");
+
+    console.group("<-- Game 3 -->");
+    playRound(getHumanChoice(prompt("rock, paper or scissors?")), getComputerChoice());
+    console.log("Human: " + humanScore + " | Computer: " + computerScore);
+    console.groupEnd();
+    console.log("\n");
+
+    console.group("<-- Game 4 -->");
+    playRound(getHumanChoice(prompt("rock, paper or scissors?")), getComputerChoice());
+    console.log("Human: " + humanScore + " | Computer: " + computerScore);
+    console.groupEnd();
+    console.log("\n");
+
+    console.group("<-- Game 5 -->");
+    playRound(getHumanChoice(prompt("rock, paper or scissors?")), getComputerChoice());
+    console.log("Human: " + humanScore + " | Computer: " + computerScore);
+    console.groupEnd();
+    console.log("\n");
 }
 
-//variables to save the score 
-let humanScore = 0; 
-let computerScore = 0;
-
-playRound(getHumanChoice(prompt("rock, paper or scissors?")), getComputerChoice());
-
-/*
-console.log("Your choice: " + getHumanChoice(prompt("rock, paper or scissors?")));
-console.log("The computer chose: " + getComputerChoice());
-*/
+playGame();
